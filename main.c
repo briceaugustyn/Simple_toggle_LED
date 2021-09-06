6dcef9e1768fcfb7a2ee0714772b89241fdf69cc
BRICE AUGUSTYN <35545541+briceaugustyn@users.noreply.github.com>
2021-09-05 18:45:45 -0700

    Update main.c

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 void
 toggleLed(unsidgned char ledMask)
 {
+    /*
     asm {
     mov dx, P2LTCH      /* Load the address of the register. */
     in  al, dx          /* Read the contnent of the register. */
@@ -25,7 +26,43 @@ toggleLed(unsidgned char ledMask)
     
     out dx, al          /*Write the nex register contnents.   */
     };
-   
+   */
+       
+       
+       
+       
+      // Init_Ports
+		movlb 0
+		clrf PORTB
+		movlb 3
+		clrf ANSELB	;	Make Port B all Digital
+		movlb 1
+		clrf TRISB	; Make Port B all output
+		movlb 0		
+		return
+
+            
+       ; These are actually called labels.  They're the only thing allowed in column 0. 
+; MPlab assumes that anything not a semicolon in column 0 is a label.
+
+LED			equ 0				; LED on pin 0 of port B
+       
+       ; Toggle the state of the LED on Port B
+Toggle_LED
+	
+	btfsc PORTB, LED	; is the LED off?
+	bra Is_On			; Go to Is_On.  Will be skipped if the LED is off
+
+Is_Off					; Executes if the LED is off
+	bsf PORTB, LED
+	return
+
+Is_On					; Executes if the LED is on
+	bcf PORTB, LED
+	return
+       
+       
+       
 };
 
 }   */ toggleLed() */
